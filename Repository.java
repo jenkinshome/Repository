a7f9b6285ff51184e01adc29b67a45572db9978a
githubjenkin <githubjenkins409@gmail.com>
2022-04-16 11:06:11 +0530

    first commit

---
+++ Repository.java
@@ -0,0 +1,11 @@
+class Repository {
+ public static void main(String args[]) {
+   System.out.println(" *  *  *");
+   System.out.println("  Sample Test Program ");
+   System.out.println(" -------------- ");
+   System.out.println(" -------------- ");
+  }
+}
+
+
+ 
